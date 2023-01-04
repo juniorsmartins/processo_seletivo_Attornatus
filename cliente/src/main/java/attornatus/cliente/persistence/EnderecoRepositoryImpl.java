c6913544837e47fc53ead1b2e2d2bd2d03d0b166
Junior Martins <juniorsoaresmartins@gmail.com>
2023-01-04 17:55:56 -0400

    Added regra de Tipo de Endereço Principal único.

--- cliente/src/main/java/attornatus/cliente/persistence/EnderecoRepositoryImpl.java
+++ cliente/src/main/java/attornatus/cliente/persistence/EnderecoRepositoryImpl.java
@@ -4,9 +4,6 @@ import attornatus.cliente.business.entities.EnderecoEntity;
 import attornatus.cliente.business.ports.PolicyEnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,7 +13,6 @@ public class EnderecoRepositoryImpl implements PolicyEnderecoRepository<Endereco
     @Autowired
     private EnderecoRepositoryJPA enderecoRepositoryJPA;
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
     @Override
     public EnderecoEntity save(EnderecoEntity entity) {
         return this.enderecoRepositoryJPA.save(entity);
